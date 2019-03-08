409b583deae8eac9dd320f2b896fbd017b14ba14
akshi-saxena <38050818+akshi-saxena@users.noreply.github.com>
2019-03-08 19:02:06 +0530

    Update faq.php

--- home/faq.php
+++ home/faq.php
@@ -16,30 +16,30 @@
 
 </head>
 <body>
-    <?php include('../include/header/mainheader.php');?>
+    <div><?php include('../include/header/mainheader.php');?></div>
 
     <!--Accordion wrapper-->
-    <div class="accordion md-accordion " id="accordionEx" role="tablist" aria-multiselectable="true"> 
+    <div class="accordion md-accordion p-md-5 m-md-5" id="accordionEx" role="tablist" aria-multiselectable="true"> 
 
         <!-- Accordion card -->
         <div class="card">
     
-        <!-- Card header -->
-        <div class="card-header" role="tab" id="headingOne1">
-            <a data-toggle="collapse" data-parent="#accordionEx" href="#collapseOne1" aria-expanded="true"
-            aria-controls="collapseOne1">
-            <h5 class="mb-0">
-                What is NAMAN? <i class="fas fa-angle-down rotate-icon"></i>
-            </h5>
-            </a>
-        </div>
-    
-        <!-- Card body -->
-        <div id="collapseOne1" class="collapse show" role="tabpanel" aria-labelledby="headingOne1" data-parent="#accordionEx">
-            <div class="card-body">
-                Naman Angels India Foundation is Navi Mumbai’s first angel network focused on facilitating investments in early stage startups. The company acts as a facilitator and undertakes secretarial functions on behalf of NAMAN members.
+            <!-- Card header -->
+            <div class="card-header" role="tab" id="headingOne1">
+                <a data-toggle="collapse" data-parent="#accordionEx" href="#collapseOne1" aria-expanded="true"
+                aria-controls="collapseOne1">
+                <h5 class="mb-0">
+                    What is NAMAN? <i class="fas fa-angle-down rotate-icon"></i>
+                </h5>
+                </a>
             </div>
-        </div>
+        
+            <!-- Card body -->
+            <div id="collapseOne1" class="collapse show" role="tabpanel" aria-labelledby="headingOne1" data-parent="#accordionEx">
+                <div class="card-body">
+                    Naman Angels India Foundation is Navi Mumbai’s first angel network focused on facilitating investments in early stage startups. The company acts as a facilitator and undertakes secretarial functions on behalf of NAMAN members.
+                </div>
+            </div>
     
         </div>
         <!-- Accordion card -->
@@ -47,22 +47,22 @@
         <!-- Accordion card -->
         <div class="card">
     
-        <!-- Card header -->
-        <div class="card-header" role="tab" id="headingTwo2">
-            <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#collapseTwo2"
-            aria-expanded="false" aria-controls="collapseTwo2">
-            <h5 class="mb-0">
-                Who are NAMAN members? <i class="fas fa-angle-down rotate-icon"></i>
-            </h5>
-            </a>
-        </div>
-    
-        <!-- Card body -->
-        <div id="collapseTwo2" class="collapse" role="tabpanel" aria-labelledby="headingTwo2" data-parent="#accordionEx">
-            <div class="card-body">
-                NAMAN Network members are successful entrepreneurs, business professionals and industry experts who want to invest in early stage companies and also support them with their expertise and networks.
+            <!-- Card header -->
+            <div class="card-header" role="tab" id="headingTwo2">
+                <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#collapseTwo2"
+                aria-expanded="false" aria-controls="collapseTwo2">
+                <h5 class="mb-0">
+                    Who are NAMAN members? <i class="fas fa-angle-down rotate-icon"></i>
+                </h5>
+                </a>
             </div>
-        </div>
+        
+            <!-- Card body -->
+            <div id="collapseTwo2" class="collapse" role="tabpanel" aria-labelledby="headingTwo2" data-parent="#accordionEx">
+                <div class="card-body">
+                    NAMAN Network members are successful entrepreneurs, business professionals and industry experts who want to invest in early stage companies and also support them with their expertise and networks.
+                </div>
+            </div>
     
         </div>
         <!-- Accordion card -->
@@ -70,22 +70,22 @@
         <!-- Accordion card -->
         <div class="card">
     
-        <!-- Card header -->
-        <div class="card-header" role="tab" id="headingThree3">
-            <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#collapseThree3"
-            aria-expanded="false" aria-controls="collapseThree3">
-            <h5 class="mb-0">
-                When do investors meet? <i class="fas fa-angle-down rotate-icon"></i>
-            </h5>
-            </a>
-        </div>
-    
-        <!-- Card body -->
-        <div id="collapseThree3" class="collapse" role="tabpanel" aria-labelledby="headingThree3" data-parent="#accordionEx">
-            <div class="card-body">
-                NAMAN currently operates chapters in Mumbai. These meetings are conducted once a month.
+            <!-- Card header -->
+            <div class="card-header" role="tab" id="headingThree3">
+                <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#collapseThree3"
+                aria-expanded="false" aria-controls="collapseThree3">
+                <h5 class="mb-0">
+                    When do investors meet? <i class="fas fa-angle-down rotate-icon"></i>
+                </h5>
+                </a>
             </div>
-        </div>
+        
+            <!-- Card body -->
+            <div id="collapseThree3" class="collapse" role="tabpanel" aria-labelledby="headingThree3" data-parent="#accordionEx">
+                <div class="card-body">
+                    NAMAN currently operates chapters in Mumbai. These meetings are conducted once a month.
+                </div>
+            </div>
     
         </div>
         <!-- Accordion card -->
