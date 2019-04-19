5367abcf92a65f346cc0c07329c5d631b7fd9dcc
Aayush Singh <37466552+aayooush@users.noreply.github.com>
2019-04-19 19:53:04 +0530

    login added

--- Signing/login.php
+++ Signing/login.php
@@ -1,46 +1,265 @@
 <?php
-require('../server.php');
-if(isset($_SESSION['StpID'])){
-	header('location: ../StartUp/index.php');
-}
-if(isset($_SESSION['InvID'])){
-	header('location: ../Investor/index.php');
-}
+    require('../server.php');
+
+
 
 ?>
 
-<html>
+
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link type="text/css" rel="stylesheet" href="css\sign.css">
-	<title>Login | NAMAN</title>
-  	<link rel="icon" href="../img/favicon.jpg" type="image/jpg" sizes="16x16">
+
+    <link href="../css/style.css" rel="stylesheet">
+
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<link href="../lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <style>
+        .container1 {
+            padding-top: 10%;
+            padding-bottom: auto;
+            background-image: linear-gradient(to bottom right,#e6f5ff, #ccebff, #66c2ff);
+        }
+        .panel-login {
+            border-color: #ccc;
+            -webkit-box-shadow: 0px 2px 3px 0px rgba(0,0,0,0.2);
+            -moz-box-shadow: 0px 2px 3px 0px rgba(0,0,0,0.2);
+            box-shadow: 0px 2px 3px 0px rgba(0,0,0,0.2);
+        }
+        .panel-login>.panel-heading {
+            color: #00415d;
+            background-color: #fff;
+            border-color: #fff;
+            text-align:center;
+        }
+        .panel-login>.panel-heading a{
+            text-decoration: none;
+            color: #666;
+            font-weight: bold;
+            font-size: 15px;
+            -webkit-transition: all 0.1s linear;
+            -moz-transition: all 0.1s linear;
+            transition: all 0.1s linear;
+        }
+        .panel-login>.panel-heading a.active{
+            color: #0066ff;
+            font-size: 18px;
+        }
+        .panel-login>.panel-heading hr{
+            margin-top: 10px;
+            margin-bottom: 0px;
+            clear: both;
+            border: 0;
+            height: 1px;
+            background-image: -webkit-linear-gradient(left,rgba(0, 0, 0, 0),rgba(0, 0, 0, 0.15),rgba(0, 0, 0, 0));
+            background-image: -moz-linear-gradient(left,rgba(0,0,0,0),rgba(0,0,0,0.15),rgba(0,0,0,0));
+            background-image: -ms-linear-gradient(left,rgba(0,0,0,0),rgba(0,0,0,0.15),rgba(0,0,0,0));
+            background-image: -o-linear-gradient(left,rgba(0,0,0,0),rgba(0,0,0,0.15),rgba(0,0,0,0));
+        }
+        .panel-login input[type="text"],.panel-login input[type="email"],.panel-login input[type="password"] {
+            height: 45px;
+            border: 1px solid #ddd;
+            font-size: 16px;
+            -webkit-transition: all 0.1s linear;
+            -moz-transition: all 0.1s linear;
+            transition: all 0.1s linear;
+        }
+        .panel-login input:hover,
+        .panel-login input:focus {
+            outline:none;
+            -webkit-box-shadow: none;
+            -moz-box-shadow: none;
+            box-shadow: none;
+            border-color: #ccc;
+        }
+        .btn-login {
+            background-color: #59B2E0;
+            outline: none;
+            color: #fff;
+            font-size: 14px;
+            height: auto;
+            font-weight: normal;
+            padding: 14px 0;
+            text-transform: uppercase;
+            border-color: #59B2E6;
+        }
+        .btn-login:hover,
+        .btn-login:focus {
+            color: #fff;
+            background-color: #53A3CD;
+            border-color: #53A3CD;
+        }
+        .forgot-password {
+            text-decoration: underline;
+            color: #888;
+        }
+        .forgot-password:hover,
+        .forgot-password:focus {
+            text-decoration: underline;
+            color: #666;
+        }
+
+        .btn-register {
+            background-color: #1CB94E;
+            outline: none;
+            color: #fff;
+            font-size: 14px;
+            height: auto;
+            font-weight: normal;
+            padding: 14px 0;
+            text-transform: uppercase;
+            border-color: #1CB94A;
+        }
+        .btn-register:hover,
+        .btn-register:focus {
+            color: #fff;
+            background-color: #1CA347;
+            border-color: #1CA347;
+        }
+    </style>
 </head>
 <body>
 
-<?php require 'include/header/sign.php'; ?>
+    <header id="header" class="fixed-top">
+      <div class="container">
+        <div class="logo float-left">
+          <a href="#intro" class="scrollto"><img src="../img/logo.png" alt="" class="img-fluid"></a>
+        </div>
 
-<div class="grid-container">
+        <nav class="main-nav float-right d-none d-lg-block">
+          <ul>
+            <li class="active"><a href="#intro">Home</a></li>
+            <li><a href="#about">About Us</a></li>
+            <li><a href="#startups">Startups</a></li>
+            <li><a href="#investors">Investors</a></li>
 
-	<div class="item2">
-		<center>
-		<img src="../img/logo.png" height="200">
-		</center>
-		Welcome to Naman
-		<hr style="align:center;size:10px;width:30%">
-	</div>
+            <li><a href="#contact">Contact Us</a></li>
+            <div class="btn-group">
+              <li><button type="button" class="btn btn-primary" onclick="location.href = './Signing/login.php'"><a style="height:40px;">Sign In</a></button></li>
+              <li>&nbsp;</li>
+              <li><button type="button" class="btn btn-primary" onclick="location.href = './Signing/register.php'"><a style="height:40px;">Sign Up</a></button></li>
+            </div>
+          </ul>
+        </nav><!-- .main-nav -->
 
-	<div class="item3">
-		Please select the role that describes you.
-		<br>
-		<button class="buttonst" onclick="location.href='login_st.php'">I'm A Start-up</button>
-		<br>
-		<button class="buttoninv" onclick="location.href='login_inv.php'">I'm An Investor</button>
-		<br>
-		<br>
-		<hr style="align:center;size:10px;width:30%">
-	</div>
-</div>
-<?php require "../include/footer/footersmall.php"?>
+      </div>
+    </header><!-- #header -->
+    <script>
+    $(function() {
+
+    $('#login-startup-link').click(function(e) {
+        $("#login-startup").delay(100).fadeIn(100);
+        $("#login-investor").fadeOut(100);
+        $('#login-investor-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+    $('#login-investor-link').click(function(e) {
+        $("#login-investor").delay(100).fadeIn(100);
+        $("#login-startup").fadeOut(100);
+        $('#login-startup-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+
+    });
+    </script>
+    <div class="container1">
+    	<div class="row">
+			<div class="col-md-4 col-md-offset-4">
+				<div class="panel panel-login">
+					<div class="panel-heading">
+						<div class="row">
+							<div class="col-xs-5 col-xs-offset-1">
+								<a href="#" class="active" id="login-startup-link">Start-up</a>
+							</div>
+							<div class="col-xs-5">
+								<a href="#" id="login-investor-link">Investor</a>
+							</div>
+						</div>
+						<hr>
+					</div>
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-lg-12">
+								<form id="login-startup" action="login.php" method="post" role="form" style="display: block;">
+									<div class="form-group">
+										<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+									</div>
+									<div class="form-group">
+										<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+									</div>
+									<!-- <div class="form-group text-center">
+										<input type="checkbox" tabindex="3" class="" name="remember" id="remember">
+										<label for="remember"> Remember Me</label>
+									</div> -->
+									<div class="form-group">
+										<div class="row">
+											<div class="col-sm-6 col-sm-offset-3">
+												<input type="submit" name="login_st" id="login_st" tabindex="4" class="form-control btn btn-login" value="Log In">
+											</div>
+										</div>
+									</div>
+									<div class="form-group">
+										<div class="row">
+											<div class="col-lg-12">
+												<div class="text-center">
+													<a href="recovermypass.php" tabindex="5" class="forgot-password">Forgot Password?</a>
+												</div>
+											</div>
+										</div>
+									</div>
+								</form>
+								<form id="login-investor" action="login.php" method="post" role="form" style="display: none;">
+									<div class="form-group">
+										<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+									</div>
+									<div class="form-group">
+										<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+									</div>
+									<!-- <div class="form-group text-center">
+										<input type="checkbox" tabindex="3" class="" name="remember" id="remember">
+										<label for="remember"> Remember Me</label>
+									</div> -->
+									<div class="form-group">
+										<div class="row">
+											<div class="col-sm-6 col-sm-offset-3">
+												<input type="submit" name="login_inv" id="login_inv" tabindex="4" class="form-control btn btn-login" value="Log In">
+											</div>
+										</div>
+									</div>
+									<div class="form-group">
+										<div class="row">
+											<div class="col-lg-12">
+												<div class="text-center">
+													<a href="https://phpoll.com/recover" tabindex="5" class="forgot-password">Forgot Password?</a>
+												</div>
+											</div>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+    </div>
 </body>
+
+<!-- <script src="../lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../lib/easing/easing.min.js"></script>
+<script src="../lib/mobile-nav/mobile-nav.js"></script>
+<script src="../lib/wow/wow.min.js"></script>
+<script src="../lib/waypoints/waypoints.min.js"></script>
+<script src="../lib/counterup/counterup.min.js"></script>
+<script src="../lib/owlcarousel/owl.carousel.min.js"></script>
+<script src="../lib/isotope/isotope.pkgd.min.js"></script>
+<script src="../lib/lightbox/js/lightbox.min.js"></script> -->
+<!-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script> -->
+
 </html>
